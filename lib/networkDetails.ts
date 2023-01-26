c28987a5e0f8a1f00f6bf389dc1744aeab87e675
daedboi <87483308+daedboi@users.noreply.github.com>
2023-01-26 20:18:31 +0200

    Added testnet contracts

--- lib/networkDetails.ts
+++ lib/networkDetails.ts
@@ -11,6 +11,9 @@ interface INetworkDetails {
     prefix: string;
     logoURI: string;
     vestingFactory: string;
+    migrator: string;
+    pills: string;
+    mpx: string;
   };
 }
 
@@ -23,6 +26,9 @@ export const networkDetails: INetworkDetails = {
     prefix: 'fantom',
     logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/fantom/info/logo.png',
     vestingFactory: CONTRACTS.VESTING_FACTORY_FANTOM,
+    migrator: CONTRACTS.MIGRATOR_FANTOM,
+    pills: '',
+    mpx: '',
   },
   4002: {
     rpcUrl: 'https://rpc.ankr.com/fantom_testnet',
@@ -32,5 +38,8 @@ export const networkDetails: INetworkDetails = {
     prefix: 'fantom',
     logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/fantom/info/logo.png',
     vestingFactory: CONTRACTS.VESTING_FACTORY_FANTOM,
+    migrator: CONTRACTS.MIGRATOR_FANTOM_TESTNET,
+    pills: '0x960A8DFb58D443f5fffac146E5b9EdE87c2bB90A',
+    mpx: '0x7a0B7eBBfc3b25Fd23182AfA43a95885B9aEca95',
   },
 };
