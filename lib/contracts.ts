4cd0747bd9e50a4f27e228632d715d3ffdad0be3
daedboi <87483308+daedboi@users.noreply.github.com>
2024-01-19 19:35:37 +0200

    Fourth airdrop

--- lib/contracts.ts
+++ lib/contracts.ts
@@ -1,1 +1,1 @@
-export const AIRDROP_CONTRACT = '0xAF944D126813044343b26Cb36f95b7406c9891d6'; // update to required airdropper contract
+export const AIRDROP_CONTRACT = '0x608EbBc40c77fa90F5f2238Ee4dfc822d5486652'; // update to required airdropper contract
